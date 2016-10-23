fb630ef5eb15bc01af78164f763c1d9456f29c89
sin <must.sow.before.you.can.reap+0001@gmail.com>
2016-10-23 15:42:02 +0900

    Update bootstrap.sh
    Fix to run by dockerhub.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,4 @@
-docker build -t python3 .
-docker run -it --name python3 python3 /bin/bash
+docker pull orgsin/python3:ver1.0
+docker images
+docker run -it orgsin/python3:ver1.0 /bin/zsh
+# docker build -t python3 .
